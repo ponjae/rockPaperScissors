735215a677397077ec5157b793f3bf2e9be481ef
ponjae <ponjae11@gmail.com>
2021-07-04 20:26:16 +0200

    Now working browser version. Styling still needed.

--- game.js
+++ game.js
@@ -1,62 +1,87 @@
 plays = ['rock', 'paper', 'scissors']
+const rock = document.getElementById('rock')
+const paper = document.getElementById('paper')
+const scissors = document.getElementById('scissors')
+const playInfo = document.getElementById('info')
+const score = document.getElementById('score')
+const final = document.getElementById('final')
+let userScore = 0
+let computerScore = 0
 
 function computerPlay() {
     index = Math.floor(Math.random() * 3)
     return plays[index]
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    computerSelection = computerPlay()
     if (playerSelection === computerSelection) {
-        console.log(`It is a draw, both players played ${playerSelection}.`)
-        return 'Draw'
+        playInfo.innerText = `It is a draw, both players played ${playerSelection}.`
     } else if (playerSelection === 'scissors') {
         if (computerSelection === 'rock') {
-            console.log(`Computer wins! ${computerSelection} beats ${playerSelection}.`)
-            return 'Computer'
+            playInfo.innerText = `Computer wins! Rock beats ${playerSelection}.`
+            computerScore += 1
         } else {
-            console.log(`You win! ${playerSelection} beats ${computerSelection}.`)
-            return 'User'
+            playInfo.innerText = `You win! Scissors beats ${computerSelection}.`
+            userScore += 1
         }
     } else if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
-            console.log(`Computer wins! ${computerSelection} beats ${playerSelection}.`)
-            return 'Computer'
+            playInfo.innerText = `Computer wins! Paper beats ${playerSelection}.`
+            computerScore += 1
         } else {
-            console.log(`You win! ${playerSelection} beats ${computerSelection}.`)
-            return 'User'
+            playInfo.innerText = `You win! Rock beats ${computerSelection}.`
+            userScore += 1
         }
     } else if (playerSelection === 'paper') {
         if (computerSelection === 'scissors') {
-            console.log(`Computer wins! ${computerSelection} beats ${playerSelection}.`)
-            return 'Computer'
-        } else {
-            console.log(`You win! ${playerSelection} beats ${computerSelection}.`)
-            return 'User'
-        }
-    }
-}
-
-function game() {
-    let userScore = 0
-    let computerScore = 0
-    for (i = 0; i < 5; i++) {
-        const userSelection = prompt('Choose your play!').toLowerCase()
-        const computerSelection = computerPlay()
-        result = playRound(userSelection, computerSelection)
-        if (result === 'User') {
-            userScore += 1
-        } else if (result === 'Computer') {
+            playInfo.innerText = `Computer wins! Scissors beats ${playerSelection}.`
             computerScore += 1
+        } else {
+            playInfo.innerText = `You win! Paper beats ${computerSelection}.`
+            userScore += 1
         }
-    }
-    console.log('**************** THE RESULT IS IN ****************')
-    if (userScore > computerScore) {
-        console.log(`You beat the computer. The final score was ${userScore}-${computerScore}`)
-    } else if (computerScore > userScore) {
-        console.log(`The computer beat you. The final score was ${userScore}-${computerScore}`)
-    } else {
-        console.log(`It´s a draw! The final score was ${userScore}-${computerScore}`)
     }
 }
 
-game()
+function updateScore() {
+    score.innerText = `${userScore} - ${computerScore}`
+}
+
+function validateWinner() {
+    if (userScore > 4 || computerScore > 4) {
+        score.style.display = 'none'
+        playInfo.style.display = 'none'
+        rock.style.display = 'none'
+        paper.style.display = 'none'
+        scissors.style.display = 'none'
+        let winner = ''
+        if (userScore > 4) {
+            winner = 'You'
+        } else {
+            winner = 'Computer'
+        }
+        final.innerText = `GAME OVER. ${winner} won. The final score was ${userScore} - ${computerScore}.`
+    }
+
+}
+
+// Event listeners
+rock.addEventListener('click', function () {
+    const play = plays[0]
+    playRound(play)
+    updateScore()
+    validateWinner()
+})
+paper.addEventListener('click', function () {
+    const play = plays[1]
+    playRound(play)
+    updateScore()
+    validateWinner()
+})
+scissors.addEventListener('click', function () {
+    const play = plays[2]
+    playRound(play)
+    updateScore()
+    validateWinner()
+})
